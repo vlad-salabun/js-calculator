6f651477f24c72ed14fdac8c99a61792782776bc
Vlad <salabunv@ukr.net>
2023-07-24 21:45:00 +0300

    Add favicon

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     entry: "./src/index.js",
@@ -18,7 +19,18 @@ module.exports = {
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    // Copy file:
+                    from: path.resolve(__dirname, './src/favicon.ico'),
+                    to: path.resolve(__dirname, './docs/favicon.ico')
+                }
+            ]
+        })
+    ]
 }
